dbfcdf76730ba42d2c07e9d87393c7fd0ba98a73
Davi Herculino <davi.herculino.ext@nuclea.com.br>
2024-01-01 14:22:50 -0300

    feat(category): changes transaction update method to only call setter if value changed

--- src/main/java/com/github/davitavora/jooq/mapper/TransactionMapper.java
+++ src/main/java/com/github/davitavora/jooq/mapper/TransactionMapper.java
@@ -4,13 +4,16 @@ import com.github.davitavora.jooq.model.representation.TransactionRepresentation
 import io.vobiscum.jooqpoc.domain.Tables;
 import io.vobiscum.jooqpoc.domain.tables.records.FinancialTransactionRecord;
 import java.util.List;
+import org.apache.commons.lang3.ObjectUtils;
 import org.jooq.DSLContext;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper
+@Mapper(imports = ObjectUtils.class)
 public abstract class TransactionMapper {
 
     @Autowired
@@ -20,11 +23,16 @@ public abstract class TransactionMapper {
 
     public abstract List<TransactionRepresentation> asRepresentation(List<FinancialTransactionRecord> projections);
 
+    @BeanMapping(ignoreByDefault = true, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "name", target = "name", conditionExpression = "java(ObjectUtils.notEqual(record.getName(), representation.name()))")
+    @Mapping(source = "detail", target = "detail", conditionExpression = "java(ObjectUtils.notEqual(record.getDetail(), representation.detail()))")
+    @Mapping(source = "amount", target = "amount", conditionExpression = "java(ObjectUtils.notEqual(record.getAmount(), representation.amount()))")
+    @Mapping(source = "categoryId", target = "categoryId", conditionExpression = "java(ObjectUtils.notEqual(record.getCategoryId(), representation.categoryId()))")
+    public abstract void update(@MappingTarget FinancialTransactionRecord record, TransactionRepresentation representation);
+
     @Mapping(target = "id", ignore = true)
     public FinancialTransactionRecord asNewRecord(TransactionRepresentation transaction) {
         return jooq.newRecord(Tables.FINANCIAL_TRANSACTION, transaction);
     }
-
-    public abstract void update(@MappingTarget FinancialTransactionRecord record, TransactionRepresentation representation);
 
 }
