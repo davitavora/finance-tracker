cde1d584ef5372f9a00f00c1e9660ae7c8dc7cd1
Davi Herculino <davi.herculino.ext@nuclea.com.br>
2024-01-01 22:43:22 -0300

    feat(error-handling): adds default exception handler

---
+++ src/main/java/com/github/davitavora/jooq/exception/DefaultExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.github.davitavora.jooq.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
--- src/main/java/com/github/davitavora/jooq/exception/ResourceFoundException.java
+++ src/main/java/com/github/davitavora/jooq/exception/ResourceFoundException.java
@@ -1,9 +1,18 @@
 package com.github.davitavora.jooq.exception;
 
-public class ResourceFoundException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.ErrorResponseException;
+
+public class ResourceFoundException extends ErrorResponseException {
 
     public ResourceFoundException(String resource, Number identifier) {
-        super("Unable to find %s %s".formatted(resource, identifier));
+        super(HttpStatus.NOT_FOUND, asProblemDetail(resource, identifier), null);
+    }
+
+    private static ProblemDetail asProblemDetail(String resource, Number identifier) {
+        final var message = "Unable to find %s %s".formatted(resource, identifier);
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, message);
     }
 
 }
